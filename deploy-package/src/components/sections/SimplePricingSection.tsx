abec5b5cc791bcee6f094970274be63e8ded3775
Usuario <usuario@MacBook-Air.local>
2025-04-17 02:47:11 -0300

    Change 'con' to 'o' in payment text below the green button

--- deploy-package/src/components/sections/SimplePricingSection.tsx
+++ deploy-package/src/components/sections/SimplePricingSection.tsx
@@ -366,7 +366,7 @@ const SimplePricingSection = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 text-[#22c55e]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                   </svg>
-                  <p className="text-[10px] text-foreground/70">{t('pagoSeguro')} PayPal {t('con')} {t('monedaLocal')}</p>
+                  <p className="text-[10px] text-foreground/70">{t('pagoSeguro')} PayPal o {t('monedaLocal')}</p>
                 </div>
               </div>
 
