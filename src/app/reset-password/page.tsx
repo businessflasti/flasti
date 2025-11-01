88c7ba834351f0eebf5118768a927300f0dcef2e
Flasti <dev@flasti.net>
2025-11-01 00:35:18 -0300

    🐛 Fix: Agregar import de Image en reset-password page

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
